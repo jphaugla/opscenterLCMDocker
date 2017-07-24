dcfe55fb0ca1520c9802fe9fd2b60991f43894d6
Jason Haugland <jasonhaugland@jhaugland-rmbp15.local>
2017-07-24 07:12:06 -0500

    update for ubuntu 16.  use entrypoint for opscenter image

--- onlyOps.sh
+++ onlyOps.sh
@@ -1,6 +1,5 @@
 echo "Run node opscenter"
 export OPSC_IMAGE=opscenter
-#  export OPSC_IMAGE=9633fa9000ec
 docker run -d --name opscenter -p 8888:8888 $OPSC_IMAGE
-docker exec opscenter service opscenterd restart
+# docker exec opscenter service opscenterd restart
 export STOMP_INTERFACE=`docker exec opscenter hostname -I`
